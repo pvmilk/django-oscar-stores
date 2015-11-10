8f7af45ce209afd6e9cadcd89847cd8c20fc5bb9
Milk Phongtharin <pvmilk@gmail.com>
2015-11-11 00:56:51 +0900

    Add OSCAR_STORES_MAIN_TEMPLATE_DIR variable, so that it can be used to load template directory in the django project.

--- stores/__init__.py
+++ stores/__init__.py
@@ -3,6 +3,6 @@ import os
 
 # Keep a setting for the path to the templates in case a project subclasses the
 # models and still needs to use the templates
-TEMPLATE_DIR = os.path.join(
+OSCAR_STORES_MAIN_TEMPLATE_DIR  = os.path.join(
     os.path.dirname(__file__),
     'templates')
